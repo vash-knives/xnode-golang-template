7eeaf481a253c2b3deb051e6ba62ab1c7117d963
Vash Knives <aclassynerd@gmail.com>
2024-11-07 23:50:07 +0800

    changes to fix the build not finding the cmd directory

--- nix/package.nix
+++ nix/package.nix
@@ -4,6 +4,9 @@
   ...
 }:
 let
+  # This was added as directories are not being copied to the clean environment that nix creates when running the buildgomodule
+  # Previously you are forced to place your go files on the root go directory (eg. where y ou find go.mod, go.sum, etc)
+  # What this does is it creates a path that points to the original source and executes from there. 
   source = builtins.path {
     name = "go-app-source";
     path = ../go-app;
@@ -20,39 +23,39 @@ in
 buildGo123Module rec {
   pname = "go-app";
   version = "1.0.0";
-  src = ../go-app;
+  # src = ../go-app;
+  src = source;
   
+  # # For any change on your app code, uncomment this (vendorHash = lib.fakehash) and comment out the other vendorHash so it generates a new hash 
+  # # which you will use to update the second vendorHash variable.
   # vendorHash = lib.fakeHash;
-  vendorHash = "sha256-3LBAOYoHID4Jy7fYyfm7b/ZSWrqbwlW/cz9CDFXDliA=";
+  vendorHash = "sha256-Heupmi9qWVq64O6d7zyLiR3nRj3D62BQ2Z81vTfTTMM=";
 
   buildPhase = ''
-    echo "Starting build phase"
+    echo "Starting build phase -- Nov 7 11:37pm"
 
     echo "Current working directory:"
     pwd
 
-    echo "Parent directory contents:"
-    ls -la ../
-
-    echo "Full directory tree from root:"
-    find / -name "cmd" 2>/dev/null
-    
-    echo "Full directory tree from current:"
-    find . -type d
-
-    echo "Source root: $sourceRoot"
-    echo "NIX_BUILD_TOP: $NIX_BUILD_TOP"
-
     echo "Current directory contents:"
     ls -la
 
-    echo "Creating a test directory:"
-    mkdir cmd-dos
-    echo "Checking again contents"
-    ls -la
+
+    # # Explicitly set the proxy. To fix the GOPROXY=off issue
+    # Ensure network access
+    export GOPROXY="https://proxy.golang.org,direct"
+    export GOSUMDB="sum.golang.org"
+    export HOME=$TMPDIR
+    
+    # Debug info
+    echo "Current GOPROXY: $GOPROXY"
+    echo "Current GOSUMDB: $GOSUMDB"
+    echo "Current HOME: $HOME"
+    
+    go mod download
 
     echo "Building Go application..."
-    go build -C cmd -v -o ${pname}
+    go build -o ${pname} ./cmd
     echo "Post-build directory contents:"
     ls -la
     echo "Build phase completed"
